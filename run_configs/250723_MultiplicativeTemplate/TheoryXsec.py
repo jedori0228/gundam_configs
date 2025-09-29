f97d2eb7cd5f36426f79bd1d993d7851a435574b
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2025-09-29 15:41:57 -0500

    Unnecessrary files removed

--- run_configs/250723_MultiplicativeTemplate/TheoryXsec.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-class TheoryXsec:
-    def __init__(self, csv_basedir):
-        self.csv_basedir = csv_basedir
-    def GetXsec(self, var):
-        csv_filepath = f'{self.csv_basedir}/xsec_{var}.csv'
-        df = pd.read_csv(csv_filepath)
-        return df['x_center'].to_numpy(), df['x_err'].to_numpy(), df['xsec'].to_numpy()
